714ecff4a6790ee29e25fa00c7f9651504b13460
Tyler Joy <mrjoy206@gmail.com>
2023-11-15 11:14:46 -0800

    added client side code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-alert('working')
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const countysName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://seattle-rent-web-api-production.up.railway.app/api/${countysName}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('h2').innerText = data.Studio
+    }catch(error){
+        console.log(error)
+    }
+}
